7c1a4cf9286874738290e3a7966cb8ac10e52fa7
lggg123 <machinelearner2334@gmail.com>
2023-11-13 11:55:23 -0800

    fix README

--- scripts/store-asset.mjs
+++ scripts/store-asset.mjs
@@ -5,25 +5,21 @@ dotenv.config()
 
 const { NFT_STORAGE_API_KEY } = process.env
 
-async function storeAssets(fileNames) {
+async function storeAsset() {
    const client = new NFTStorage({ token: NFT_STORAGE_API_KEY })
-   for (const fileName of fileNames) {
-    const metadata = await client.store({
-        name: fileName,
-        description: `A 3D generated image of ${fileName}`,
-        image: new File(
-            [await fs.promises.readFile(`assets/${fileName}.png`)],
-            `${fileName}.png`,
-            { type: 'image/png' }
-        ),
-    });
-    console.log(`Metadata for ${fileName} stored on Filecoin and IPFS with URL: ${metadata.url}`);
-   }
+   const metadata = await client.store({
+       name: 'Clowndownze',
+       description: 'A NFT pixel clown',
+       image: new File(
+           [await fs.promises.readFile('assets/Clowndownze.png')],
+           'Clowndownze.png',
+           { type: 'image/png' }
+       ),
+   })
+   console.log("Metadata stored on Filecoin and IPFS with URL:", metadata.url)
 }
 
-const fileNames = ["Palm Beach House #1", "Palm Beach House #2", "Palm Beach House #3", "Palm Beach House #4", "Palm Beach House #5", "Palm Beach House #6", "Palm Beach House #7", "Palm Beach House #8", "Palm Beach House #9", "Palm Beach House #10"];
-
-storeAssets(fileNames)
+storeAsset()
    .then(() => process.exit(0))
    .catch((error) => {
        console.error(error);
